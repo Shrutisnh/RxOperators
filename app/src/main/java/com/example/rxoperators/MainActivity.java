1b24263b2d933e3b0a19c5fe73bad8a00209a5c1
Shruti <shruti_sinha@persistent.com>
2021-04-05 22:50:38 +0530

    Buffer operator example

--- app/src/main/java/com/example/rxoperators/MainActivity.java
+++ app/src/main/java/com/example/rxoperators/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.rxoperators;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import com.jakewharton.rxbinding3.view.RxView;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import kotlin.Unit;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -20,159 +27,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Create Operator for a Single Observable
+       /* Observable<Task> taskObservable = Observable
+                .fromIterable(DataSource.createTasksList())
+                .subscribeOn(Schedulers.io());
 
- /*       final  Task task = new Task("Walk the dog.",3,false);
-        Observable<Task> taskObservable = Observable
-                .create(new ObservableOnSubscribe<Task>() {
+        taskObservable
+                .buffer(2) // Apply the Buffer() operator
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<Task>>() { // Subscribe and view the emitted results
                     @Override
-                    public void subscribe(@NonNull ObservableEmitter<Task> emitter) throws Exception {
-                    if(!emitter.isDisposed()){
-                        emitter.onNext(task);
-                        emitter.onComplete();
+                    public void onSubscribe(Disposable d) {
                     }
-                    }
-                })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
-
-        taskObservable.subscribe(new Observer<Task>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
-                Log.d(TAG, "onSubscribe: ");
-            }
-
-            @Override
-            public void onNext(@NonNull Task task) {
-                Log.d(TAG, "onNext: "+task.getDescription());
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
-
-  */
-//Create Operator for list observable
-
-   /*             Observable<Task> taskObservable = Observable
-                .create(new ObservableOnSubscribe<Task>() {
                     @Override
-                    public void subscribe(@NonNull ObservableEmitter<Task> emitter) throws Exception {
-                        for (Task task : DataSource.createTasksList()) {
-                            if (!emitter.isDisposed()) {
-                                emitter.onNext(task);
-                            }
-
-                        }
-                        if(!emitter.isDisposed()){
-                            emitter.onComplete();
+                    public void onNext(List<Task> tasks) {
+                        Log.d(TAG, "onNext: bundle results: -------------------");
+                        for(Task task: tasks){
+                            Log.d(TAG, "onNext: " + task.getDescription());
                         }
                     }
-                })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
 
-        taskObservable.subscribe(new Observer<Task>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
-                Log.d(TAG, "onSubscribe: ");
-            }
-
-            @Override
-            public void onNext(@NonNull Task task) {
-                Log.d(TAG, "onNext: "+task.getDescription());
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
-        */
-
-        //range operator
-
-     /*   Observable<Task> observable = Observable
-                .range(0,9)
-                .subscribeOn(Schedulers.io())
-                .map(new Function<Integer, Task>() {
                     @Override
-                    public Task apply(@NonNull Integer integer) throws Exception {
-                        return new Task("This is a task with priority"+String.valueOf(integer),integer,true);
+                    public void onError(Throwable e) {
+                    }
+                    @Override
+                    public void onComplete() {
+                    }
+                });*/
+
+        RxView.clicks(findViewById(R.id.button))
+                .map(new Function<Unit, Integer>() {
+                    @Override
+                    public Integer apply(@NonNull Unit unit) throws Exception {
+                        return 1;
                     }
                 })
-                .takeWhile(new Predicate<Task>() {
+                .buffer(2, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<Integer>>() {
                     @Override
-                    public boolean test(@NonNull Task task) throws Exception {
-                        return task.getPriority()<9;
+                    public void onSubscribe(@NonNull Disposable d) {
+
                     }
-                })
-                .observeOn(AndroidSchedulers.mainThread());
 
-        observable.subscribe(new Observer<Task>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
+                    @Override
+                    public void onNext(@NonNull List<Integer> integers) {
+                        Log.d(TAG, "onNext: You clicked "+integers.size()+" times in 4 seconds" );
+                    }
 
-            }
+                    @Override
+                    public void onError(@NonNull Throwable e) {
 
-            @Override
-            public void onNext(@NonNull Task task) {
-                Log.d(TAG, "onNext: "+task.getDescription());
-            }
+                    }
 
-            @Override
-            public void onError(@NonNull Throwable e) {
+                    @Override
+                    public void onComplete() {
 
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
-
-      */
-        //Repeat Operator
-
-        Observable<Integer> observable = Observable
-                .range(0,9)
-                .subscribeOn(Schedulers.io())
-                .repeat(3)
-                .observeOn(AndroidSchedulers.mainThread());
-
-        observable.subscribe(new Observer<Integer>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
-
-            }
-
-            @Override
-            public void onNext(@NonNull Integer integer) {
-                Log.d(TAG, "onNext: "+integer);
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
+                    }
+                });
 
     }
     }
